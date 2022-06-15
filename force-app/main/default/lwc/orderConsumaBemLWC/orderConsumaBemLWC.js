43d7236cf72181ac886546c229b9bad7fb68ff7f
Bruna Girão <brunagirao.s@gmail.com>
2022-06-15 16:46:36 -0300

    orderConsumaBemLWC - add getAccountName and getAccountAddresses methods

--- force-app/main/default/lwc/orderConsumaBemLWC/orderConsumaBemLWC.js
+++ force-app/main/default/lwc/orderConsumaBemLWC/orderConsumaBemLWC.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 
-export default class OrderConsumaBemLWC extends LightningElement {}
+import getAccountName from '@salesforce/apex/AccountController.getAccountName';
+import getAccountAddresses from '@salesforce/apex/AccountController.getAccountAddresses';
+
+export default class OrderConsumaBemLWC extends LightningElement {
+
+    @api recordId;
+    @track accountName;
+    @track accountAddresses = [];
+
+    connectedCallback() {
+
+        console.log('GET ACCOUNT DATA | ID => ', this.recordId);
+        this.getAccountName();
+        this.getAccountAddresses();
+    }
+
+    
+    async getAccountName() {
+
+        let response = await getAccountName({
+            accountId: this.recordId
+        });
+
+        this.accountName = response;
+    }
+
+    async getAccountAddresses() {
+
+        let response = await getAccountAddresses({
+            accountId: this.recordId
+        });
+        console.log('Response => ', response);
+        this.accountAddresses = response;
+
+    }
+
+    
+}
