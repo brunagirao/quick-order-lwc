f0439b0df1ca67a8845aa9a68e8fec5d3a459ec6
Bruna Girão <brunagirao.s@gmail.com>
2022-06-15 17:19:33 -0300

    PaymentConditionsController - add getPaymentConditions method

--- force-app/main/default/lwc/orderConsumaBemLWC/orderConsumaBemLWC.js
+++ force-app/main/default/lwc/orderConsumaBemLWC/orderConsumaBemLWC.js
@@ -2,18 +2,21 @@ import { LightningElement, api, track, wire } from 'lwc';
 
 import getAccountName from '@salesforce/apex/AccountController.getAccountName';
 import getAccountAddresses from '@salesforce/apex/AccountController.getAccountAddresses';
+import getPaymentConditions from '@salesforce/apex/PaymentConditionsController.getPaymentConditions';
 
 export default class OrderConsumaBemLWC extends LightningElement {
 
     @api recordId;
     @track accountName;
-    @track accountAddresses = [];
+    @track accountAddresses  = [];
+    @track paymentConditions = [];
 
     connectedCallback() {
 
         console.log('GET ACCOUNT DATA | ID => ', this.recordId);
         this.getAccountName();
         this.getAccountAddresses();
+        this.getPaymentConditions();
     }
 
     
@@ -34,6 +37,11 @@ export default class OrderConsumaBemLWC extends LightningElement {
         console.log('Response => ', response);
         this.accountAddresses = response;
 
+    }
+
+    async getPaymentConditions() {
+        let response = await getPaymentConditions();
+        this.paymentConditions = response;
     }
 
     
