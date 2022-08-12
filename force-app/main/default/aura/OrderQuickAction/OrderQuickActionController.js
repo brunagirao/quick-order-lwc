a929ba66bff2e3c1cb92c7fbf4f787897c631285
Bruna Girão <brunagirao.s@gmail.com>
2022-08-12 09:23:25 -0300

    OrderQuickAction - aura component

--- force-app/main/default/aura/OrderQuickAction/OrderQuickActionController.js
+++ force-app/main/default/aura/OrderQuickAction/OrderQuickActionController.js
@@ -1,5 +1,18 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit: function (component, event, helper) {
+        
+        var accountId = component.get("v.recordId") != undefined ? ((component.get("v.recordId")).substring(0, 3) == '001' ? component.get("v.recordId") : null) : undefined;
+
+            var url = '/lightning/n/NewOrder?';
+
+				url += `c__recordId=${accountId}`;
+
+			var urlEvent = $A.get("e.force:navigateToURL");
+			urlEvent.setParams({
+				"url": url
+			});
+
+			urlEvent.fire();
 
     }
 })
